d12531c01c0ef7631c49b71effcdcefcf3c33460
Andres Perez Quinchia <aperezq@eafit.edu.co>
2026-02-17 18:27:46 -0500

    First commit

---
+++ resources/views/home/contact.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('title', 'Contact Information')
+@section('subtitle', 'Contact Information')
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-lg-4 ms-auto">
+      <p class="lead">{{ 'Andrés Pérez Quinchía' }}</p>
+    </div>
+    <div class="col-lg-4 me-auto">
+      <p class="lead">{{ 'Carrera 49 N° 7 Sur-50' }}</p>
+    </div>
+    <div class="col-lg-4 me-auto">
+      <p class="lead">{{ '(57) 604 2619500' }}</p>
+    </div>
+  </div>
+</div>
+@endsection
---
+++ resources/views/product/save.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section("title", 'Product created')
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">Product created successfully mate!</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
